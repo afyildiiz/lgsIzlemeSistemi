f8e2e8909459ad0c5d8162c3c921b2e1553ed586
fatih <afyildiz@gmail.com>
2023-10-09 20:49:49 +0300

    config

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
     production: false,
-    baseUrl: 'http://demo.bussion.com',
-    //baseUrl: 'http://192.168.42.164',
+    // baseUrl: 'http://demo.bussion.com',
+    baseUrl: 'http://192.168.42.164',
     imageUrl: 'http://demo.bussion.com',
 
 };
